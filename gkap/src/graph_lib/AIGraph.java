a4050e59273dcd928864b58e49729ec903bf22ff
sebastian <sebastian.scheele@haw-hamburg.de>
2013-09-30 17:00:33 +0200

    erster konstruktor

--- gkap/src/graph_lib/AIGraph.java
+++ gkap/src/graph_lib/AIGraph.java
@@ -1,5 +1,34 @@
 package graph_lib;
 
 public class AIGraph {
+	public static AIGraph init() {
+		return new AIGraph();
+	}
+	
+	//Konstruktoren:
+		//– new AIGraph()
+		 // post: erzeugt einen leeren Graph
+		// returns: ID des Graphen
+//		– graph.addVertex(newItem)
+		 // post: fügt dem Graphen eine neue Ecke mit Namen
+		// newItem zu.
+		// returns: ID der Ecke
+//		– graph.deleteVertex( v-Id)
+		 // post: löscht die Ecke mit ID v-ID aus dem Graphen
+		// und alle zu ihr inzidenten Kanten.
+//		– graph.addEdgeU(v1, v2 )
+		 // post: fügt dem Graphen eine neue ungerichtete Kante
+		// zwischen v1 und v2 (gegeben als ID) zu.
+		// returns: ID dieser Kante
+//		– graph.addEdgeD(v1, v2 )
+		 // post: fügt dem Graphen eine neue gerichtete Kante
+		// von v1 nach v2 (gegeben als ID) zu.
+		// returns: ID dieser Kante
+//		– graph.deleteEdge(v1, v2 )
+		 // post: löscht die Kante zwischen den beiden gegebenen
+		// Ecken; wenn er gerichtet ist, die von v1 nach v2
+		// (gegeben als ID)
+	
+
 
 }
