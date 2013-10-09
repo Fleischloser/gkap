26f36644f825296db86e361f19069d27afa7fef4
Fleischloser <arne.k.schulz@googlemail.com>
2013-10-09 15:26:45 +0200

    ..

--- gkap/src/tests/AIGraphTest.java
+++ gkap/src/tests/AIGraphTest.java
@@ -11,21 +11,27 @@ public class AIGraphTest {
 		AIGraph g = AIGraph.init();
 		
 		String[] attrNames = {"distance"};
+		String graphName = "graph_01";
 		
-		g = Importer.importExample(g, "graph_01", attrNames);
+		g = Importer.importExample(g, graphName, attrNames);
 		
-		List<String> nodeList = g.getVertexes();
-		for (String node : nodeList) {
-			System.out.println("Node:"+node);
-			
-			List<String> edgeList = g.getIncident(node);
-			for (String edge : edgeList) {
-				String target = g.getTarget(edge);
-				if (target.equals(node)) {
-					target = g.getSource(edge);
+		if (g != null) {
+			System.out.println(graphName+":");
+			List<String> nodeList = g.getVertexes();
+			for (String node : nodeList) {
+				System.out.println("Node:"+node);
+				
+				List<String> edgeList = g.getIncident(node);
+				for (String edge : edgeList) {
+					String target = g.getTarget(edge);
+					if (target.equals(node)) {
+						target = g.getSource(edge);
+					}
+					System.out.println("  ->Edge:"+target+" "+attrNames[0]+":"+g.getValE(edge, attrNames[0]));	
 				}
-				System.out.println("  ->Edge:"+target+" "+attrNames[0]+":"+g.getValE(edge, attrNames[0]));	
 			}
+		} else {
+			System.err.println("Der Graph konnte nicht importiert werden");
 		}
 		
 	}
